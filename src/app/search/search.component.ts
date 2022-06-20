f90ba44321b7b042befe1da36066e88c83b6eb00
Kanimozhi M <kanimozhi9052@gmail.com>
2022-06-20 16:11:47 +0530

    search Panel changes

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -24,41 +24,24 @@ export class SearchComponent implements OnInit {
   userForm: FormGroup;          
   ngOnInit() {
     this.userForm = new FormGroup({
-      name: new FormControl('', [Validators.required, Validators.minLength(5)]),
-      associateId: new FormControl('', [Validators.required, Validators.minLength(3)]),
-      mobile: new FormControl('', [Validators.required, Validators.minLength(10)]),     
-      email: new FormControl('', [Validators.required]),
-      html_Css_Javascript: new FormControl('', [Validators.required]),
-      angular: new FormControl('', [Validators.required]),
-      react: new FormControl('', [Validators.required]),
-      spring: new FormControl('', [Validators.required]),
-      restful: new FormControl('', [Validators.required]),
-      hibernate: new FormControl('', [Validators.required]),
-      git: new FormControl('', [Validators.required]),
-      docker: new FormControl('', [Validators.required]),
-      jenkins: new FormControl('', [Validators.required]),
-      aws: new FormControl('', [Validators.required]),
-      spoken: new FormControl('', [Validators.required]),
-      communication: new FormControl('', [Validators.required]),
-      aptitude: new FormControl('', [Validators.required]),
+      searchVal: new FormControl('', [Validators.required]),
+
       //userType: new FormControl(),
       //startDate: new FormControl(this.datePipe.transform(this.currentDate, 'yyyy-MM-dd'))
     // }, {
     //   validator: MustMatch('name','associateId')
   });
   this.dataList = [
-    { code: 1, name: "shohel" },
-    { code: 2, name: "rana" },
-    { code: 3, name: "shipon" }
+    { code: 1, name: "AssociateId" },
+    { code: 2, name: "Name" },
+    { code: 3, name: "Skill" }
   ]
   }
 
   onSubmit(){
 
-    this.profile.name = this.userForm.value.name;
-    this.profile.associateId = this.userForm.value.associateId;
-    this.profile.mobile = this.userForm.value.mobile;
-    this.profile.email = this.userForm.value.email;
+    this.profile.name = this.userForm.value.searchVal;
+
     this.save();
   }
 
